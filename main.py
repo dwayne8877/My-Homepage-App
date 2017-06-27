b88788cfac1d4793c3934645258f4e7cac37ce72
Darrell Wayne <darrellwayneenterprises@gmail.com>
2017-06-27 06:54:27 -0500

    Post method not working

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask,request,redirect
+import cgi
+import os
+import jinja2
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+autoescape= True)
+
+#@app.route("/")
+#def index():
+#    return "Hello World"
+
+@app.route("/")
+def index():
+    template = jinja_env.get_template('userpage.html')
+    return template.render()
+
+newlinks= []
+
+@app.route("/newlink", methods=['POST', 'GET'])
+def newlink():
+
+    if request.method == 'POST':
+        link= request.form['link']
+        newlinks.append(link)
+
+    template = jinja_env.get_template('userpage.html')
+    return template.render(title='newlink', links=links)
+
+
+app.run()
