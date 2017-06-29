aed5cc9ad387e5ff61d59c5fde02f9fc659567ec
Darrell Wayne <darrellwayneenterprises@gmail.com>
2017-06-29 00:07:19 -0500

    CSS is working(created static folder and linked path). Add link feature is working. Need to add the ability to link to urls. Add form vailidation. Add database. Add remove link feature.

--- main.py
+++ main.py
@@ -13,7 +13,19 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('userpage.html', title= 'Signup')
+    return render_template('index.html', title= 'Signup')
+
+@app.route("/login")
+def login():
+    return render_template('login.html', titile= 'Login')
+
+@app.route("/signup")
+def signup():
+    return render_template('index.html', titile= 'Signup')
+
+@app.route("/logout")
+def logout():
+    return render_template('index.html')
 
 
 links= []
@@ -24,8 +36,8 @@ def newlink():
     if request.method == 'POST':
         link= request.form['link']
         links.append(link)
-
-    return render_template('userpage.html', links=links)
+    #username= request.form['username']
+    return render_template('userpage.html', links=links)#username=username)
 
 
 app.run()
